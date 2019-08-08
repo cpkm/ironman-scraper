574f713b62635ca03fcfee61751cb67487ee09ff
Kyle Air <20861192+cpkm@users.noreply.github.com>
2019-08-08 18:41:36 -0400

    ran canada140 full_scrape

--- ironman_scraper.py
+++ ironman_scraper.py
@@ -8,12 +8,13 @@ import os.path
 from bs4 import BeautifulSoup
 from athlete_scraper import simple_scrape, get_event_links, scrape_athlete_links, full_scrape
 
-results_link = 'http://www.ironman.com/triathlon/events/emea/ironman-70.3/haugesund/results.aspx'
+results_link = 'https://www.ironman.com/triathlon/events/americas/ironman/canada/results.aspx'
+#'http://www.ironman.com/triathlon/events/emea/ironman-70.3/haugesund/results.aspx'
 #'http://www.ironman.com/triathlon/events/americas/ironman-70.3/muskoka/results.aspx'
 
 #simple_scrape(results_link, outfile='data/data4')
 #scrape_athlete_links(results_link, outfile='data/data2')
 
-ad = pd.read_csv('data/haugesund703/athlete_links.csv')
+#ad = pd.read_csv('data/haugesund703/athlete_links.csv')
 
-full_scrape(results_link, linkdf=ad, data_outfile='data/haugesund703/data', link_outfile='data/haugesund703/athlete_links')
+full_scrape(results_link, data_outfile='data/canada140/data', link_outfile='data/canada140/athlete_links')
